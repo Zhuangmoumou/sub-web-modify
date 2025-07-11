1d36ca363e94f787ea1e3e1b9c9010c16c0ee239
Zhuangmoumou <103405023+Zhuangmoumou@users.noreply.github.com>
2025-07-11 23:09:58 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,6 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "自建": "https://subapi.987696.xyz"
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
